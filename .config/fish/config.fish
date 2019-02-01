87ee0fdfe17cb35de6906272afd85b5081017374
Stefan Wikner <stefan.wikner.3@consultant.volvo.com>
2019-02-01 15:20:29 +0100

    Remove not used stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,5 @@
-set arken_ip ~/Sync/arken_ip.txt
-
-if [ -f $arken_ip ]
-    set -x ARKEN (cat $arken_ip)
-end
-
 # Set vim as standard editor
 set -x VISUAL vim
 set -x EDITOR "$VISUAL"
-
-#set -gx WORKON_HOME $HOME/.virtual-environments/
-# . ~/.config/fish/workon_funcs.fish
 
 fish_vi_key_bindings
