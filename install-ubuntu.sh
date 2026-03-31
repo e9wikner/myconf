fbe121453388391916b4980c3d7cbe41467f7685
Stefan Wikner <stefan.wikner@volvocars.com>
2026-03-31 08:29:08 +0200

    Use https for astronvim repo cloning

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -25,7 +25,7 @@ sudo snap install nvim --classic
 [ -d "$HOME/.local/share/nvim" ] && mv "$HOME/.local/share/nvim" "$HOME/.local/share/nvim.bak"
 [ -d "$HOME/.local/state/nvim" ] && mv "$HOME/.local/state/nvim" "$HOME/.local/state/nvim.bak"
 [ -d "$HOME/.cache/nvim" ] && mv "$HOME/.cache/nvim" "$HOME/.cache/nvim.bak"
-git clone git@github.com:e9wikner/astronvim-config.git ~/.config/nvim
+git clone https://github.com/e9wikner/astronvim-config.git ~/.config/nvim
 
 # lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
